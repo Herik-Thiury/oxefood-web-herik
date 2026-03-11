cea60337f772aeb8773a2e708f774e53936a11a2
Hérik Thiury <herikthiury2017@gmail.com>
2026-03-11 19:31:30 -0300

    edição-cliente

--- src/views/cliente/ListCliente.jsx
+++ src/views/cliente/ListCliente.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Container, Divider, Icon, Table } from "semantic-ui-react";
+import { Button, Container, Divider, Header, Icon, Modal, Table } from "semantic-ui-react";
 import MenuSistema from "../../MenuSistema";
 
 export default function ListCliente() {
   const [lista, setLista] = useState([]);
-
+  const [openModal, setOpenModal] = useState(false);
+  const [idRemover, setIdRemover] = useState();
   useEffect(() => {
     carregarLista();
   }, []);
@@ -24,6 +25,31 @@ export default function ListCliente() {
     let arrayData = dataParam.split("-");
     return arrayData[2] + "/" + arrayData[1] + "/" + arrayData[0];
   }
+
+  function confirmaRemover(id) {
+    setOpenModal(true);
+    setIdRemover(id);
+  }
+
+  async function remover() {
+
+       await axios.delete('http://localhost:8080/api/cliente/' + idRemover)
+       .then((response) => {
+ 
+           console.log('Cliente removido com sucesso.')
+ 
+           axios.get("http://localhost:8080/api/cliente")
+           .then((response) => {
+               setLista(response.data)
+           })
+       })
+       .catch((error) => {
+           console.log('Erro ao remover um cliente.')
+       })
+       setOpenModal(false)
+   }
+
+
   return (
     <div>
       <MenuSistema tela={"cliente"} />
@@ -77,7 +103,14 @@ export default function ListCliente() {
                         title="Clique aqui para editar os dados deste cliente"
                         icon
                       >
-                        <Icon name="edit" />
+                        <Link
+                          to="/form-cliente"
+                          state={{ id: cliente.id }}
+                          style={{ color: "green" }}
+                        >
+                          {" "}
+                          <Icon name="edit" />{" "}
+                        </Link>
                       </Button>{" "}
                       &nbsp;
                       <Button
@@ -86,6 +119,7 @@ export default function ListCliente() {
                         color="red"
                         title="Clique aqui para remover este cliente"
                         icon
+                        onClick={(e) => confirmaRemover(cliente.id)}
                       >
                         <Icon name="trash" />
                       </Button>
@@ -97,6 +131,33 @@ export default function ListCliente() {
           </div>
         </Container>
       </div>
+      <Modal
+        basic
+        onClose={() => setOpenModal(false)}
+        onOpen={() => setOpenModal(true)}
+        open={openModal}
+      >
+        <Header icon>
+          <Icon name="trash" />
+          <div style={{ marginTop: "5%" }}>
+            {" "}
+            Tem certeza que deseja remover esse registro?{" "}
+          </div>
+        </Header>
+        <Modal.Actions>
+          <Button
+            basic
+            color="red"
+            inverted
+            onClick={() => setOpenModal(false)}
+          >
+            <Icon name="remove" /> Não
+          </Button>
+          <Button color="green" inverted onClick={() => remover()}>
+            <Icon name="checkmark" /> Sim
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 }
